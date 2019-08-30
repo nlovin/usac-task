4b2f8136967d5bb0d6593aac293fcee3eb926cb8
Nathan Lovin <nlovin@umd.edu>
2019-08-30 18:14:52 -0400

    created setup file
    setup file checks and loads packages and creates data dir

---
+++ setup.R
@@ -0,0 +1,34 @@
+### Setup File
+
+# check.packages function: install and load multiple R packages.
+# Check to see if packages are installed. Install them if they are not, then load them into the R session.
+check.packages <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+
+# install packages
+packages<-c("tidyverse","RSocrata", "fs")
+check.packages(packages)
+
+rm(check.packages, packages)
+
+
+
+#Check if the folder "Data" exists in the current directory, if not creates it
+#ifelse(!dir.exists("Data"), dir.create("Data"), "Folder exists already")
+
+if(!dir_exists("Data")) {
+  
+  dir_create("Data")
+  print("Data directory created")
+  
+} else {
+  
+  print("Data folder already exists")
+  
+}
+
